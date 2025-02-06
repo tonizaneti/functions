0b1781c6ec8741d33624e9589fadecc7a69cd252
Antonio Delvair Zaneti <antonio.zaneti@gmail.com>
2025-02-06 18:41:56 -0300

    Update horagmt.php

--- horagmt.php
+++ horagmt.php
@@ -1,16 +1,11 @@
 <?php
 //$hora = htmlspecialchars($_POST['hora'], ENT_QUOTES, 'UTF-8');
 $ndata = date("Y-m-d H:i:s");
-//$hora = date("H:i:s");
-//2025-12-12 18:18:36
-//18:18:36
+
 echo "Data Original: ".$ndata."<br>";
-//$novadata = substr($ndata, 8, 2) . "/" . substr($ndata, 5, 2) . "/" . substr($ndata, 0, 4);
-//$novadata = substr($ndata, 0, 4)."-".substr($ndata, 5, 2)."-".substr($ndata, 8, 2) ;
 $novahora = substr($ndata, 11, 2) . "h" . substr($ndata, 14, 2)  . "m" . substr($ndata, 17, 2) . "s";
 echo "Data Modificada: " . $novadata. "<br>";
 echo "Hora BR". $novahora."<br>";
-//$novahora=substr($hora,0,2) ."h" .substr($hora,3,2)  ."m" .substr($hora,6,2) . "s" ;
 $horabr = substr($novahora, 0, 2);
 $horaconv = (int)$horabr;
 if($horaconv >=21){
@@ -31,12 +26,11 @@ echo "Horario GMT " . $horariogmt. "<br>";
 echo "Data UTC ". $novadata_utc . "<br>";
 
 
-echo "NOvo Dia " . $novodiagmt . "<br>";
-//$novadata = substr($ndata, 8, 2) . "/" . substr($ndata, 5, 2) . "/" . substr($ndata, 0, 4);
+echo "Novo Dia " . $novodiagmt . "<br>";
 echo  "Nova hora BR: $novahora";
 echo "<br>";
 echo "Hora GMT: $horariogmt";
 $datagmt =  $novadata . $horariogmt;
-//echo "Hora GMT". $datagmt;
+
 
 ?>
